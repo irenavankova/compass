ff0ead7d641bc9149642817c16c0c0bdae5dc6af
Irena Vankova <irena.vanek@gmail.com>
2023-12-07 17:24:43 -0700

    eddy and vertvisc in outputs + more plots modify

--- compass/ocean/tests/isomip_plus/plot_sim_outputs.py
+++ compass/ocean/tests/isomip_plus/plot_sim_outputs.py
@@ -19,19 +19,20 @@ work_dir = current_working_directory
 
 mesh_array = f'{work_dir}/init.nc'
 experiment = 'Ocean0'
+dsMesh = xarray.open_dataset(mesh_array)
 
-for yr in range(1, 1, 1):
+for yr in range(0, 2, 1):
     yy = f'{yr}'.zfill(2)
-    for k in range(2, 2, 1):
+    for k in range(1, 12, 1):
         #Get snapshots from restart files
         mm = f'{k}'.zfill(2)
         output_array = f'{work_dir}/restarts/restart.00{yy}-{mm}-01_00.00.00.nc'
         if os.path.exists(output_array):
+            print(output_array)
 
             # plot a few fields
-            plot_folder = f'{work_dir}/plots_rest'
+            plot_folder = f'{work_dir}/plots/restart'
 
-            dsMesh = xarray.open_dataset(mesh_array)
             ds = xarray.open_dataset(output_array)
 
             section_y = float(40000)
@@ -52,5 +53,45 @@ for yr in range(1, 1, 1):
             plotter.plot_3d_field_top_bot_section(
                 ds.salinity, nameInTitle='salinity', prefix='salin', suffix=f'{yy}-{mm}',
                 units='PSU', vmin=33.8, vmax=34.7, cmap='cmo.haline')
+
+        output_array = f'{work_dir}/timeSeriesStatsMonthly.00{yy}-{mm}-01.nc'
+        if os.path.exists(output_array):
+            print(output_array)
+
+            # plot a few fields
+            plot_folder = f'{work_dir}/plots/monthly'
+
+            ds = xarray.open_dataset(output_array)
+
+            section_y = float(40000)
+            # show progress only if we're not writing to a log file
+            #show_progress = self.log_filename is None
+            plotter = MoviePlotter(inFolder=work_dir,
+                                   streamfunctionFolder=work_dir,
+                                   outFolder=plot_folder, sectionY=section_y,
+                                   dsMesh=dsMesh, ds=ds, expt=experiment,
+                                   showProgress=False)
+
+            #plotter.plot_horiz_series(ds.ssh, 'ssh', 'ssh',
+            #                          True, vmin=-700, vmax=0)
+            plotter.plot_3d_field_top_bot_section(
+                ds.timeMonthly_avg_potentialDensity, nameInTitle='potential density', prefix='pdens', suffix=f'{yy}-{mm}',
+                units='kg/m^3', vmin=1027., vmax=1028., cmap='cmo.thermal')
+
+            plotter.plot_3d_field_top_bot_section(
+                ds.timeMonthly_avg_activeTracers_temperature, nameInTitle='temperature', prefix='temp', suffix=f'{yy}-{mm}',
+                units='C', vmin=-2., vmax=1., cmap='cmo.thermal')
+
+            plotter.plot_3d_field_top_bot_section(
+                ds.timeMonthly_avg_activeTracers_salinity, nameInTitle='salinity', prefix='salin', suffix=f'{yy}-{mm}',
+                units='PSU', vmin=33.8, vmax=34.7, cmap='cmo.haline')
+
+            #plotter.plot_3d_field_top_bot_section(
+            #    ds.timeMonthly_avg_activeTracers_salinity, nameInTitle='salinity', prefix='salin', suffix=f'{yy}-{mm}',
+            #    units='PSU', vmin=33.8, vmax=34.7, cmap='cmo.haline')
+
+            #plotter.plot_3d_field_top_bot_section(
+            #    ds.timeMonthly_avg_vertVelocityTop, nameInTitle='vertical velocity', prefix='vertVel', suffix=f'{yy}-{mm}',
+            #    units='m/s', vmin=-0.000001, vmax=0.000001, cmap='cmo.haline')
 
 
